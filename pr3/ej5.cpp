b533a429a447f90281eb0ebbd1e2456867d01ab7
jsainero <jsainero@ucm.es>
2019-12-11 16:22:56 +0100

    Listo ej5

--- pr3/ej5.cpp
+++ pr3/ej5.cpp
@@ -1,6 +1,9 @@
 #include <iostream>
+#include <stdio.h>
 #include <sys/types.h>
 #include <unistd.h>
+#include <sys/time.h>
+#include <sys/resource.h>
 using namespace std;
 
 int main (){
@@ -8,7 +11,17 @@ int main (){
     pid_t ppid = getppid();
     pid_t pgrp = getpgrp();
     pid_t psid = getsid(0);
-    
+    char buf[300];
+    getcwd(buf,300);
+    struct rlimit rlim;
+    getrlimit(RLIMIT_NOFILE,&rlim);
+    cout << "Identificador del proceso: " << pid << '\n';
+    cout << "Identificador del proceso padre: " << ppid << '\n';
+    cout << "Identificador del grupo: " << pgrp << '\n';
+    cout << "Identificador de sesion: " << psid << '\n';
+    printf("Numero maximo de archivos: %d\n",rlim.rlim_max);
+    printf("Directorio de trabajo actual: %s\n",buf);
+
 
 
 
