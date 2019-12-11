299ef0515093a7aa45b6fd6fb308e343fa854098
jsainero <jsainero@ucm.es>
2019-12-11 17:22:08 +0100

    Create ej7a.cpp

---
+++ pr3/ej7a.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <stdlib.h>
+
+using namespace std;
+
+int main(int argc, char *argv[]){
+    system(argv[1]);
+    cout << "El comando termino de ejecutarse\n";
+    return 0;
+}
