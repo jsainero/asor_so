5a8fd848ac99d889a49341fa96b75202ef2e1198
jsainero <jsainero@ucm.es>
2019-12-16 11:47:24 +0100

    ácido

--- pr4/ej2.cpp
+++ pr4/ej2.cpp
@@ -4,19 +4,16 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <string.h>
+#include <iostream>
+using namespace std;
 
 int main(int argc, char *argv[])
 {
     int p_h[2];
     int h_p[2];
     pid_t cpid;
-    char buf;
-
-    if (argc < 5)
-    {
-        fprintf(stderr, "Usage: %s <string>\n", argv[0]);
-        exit(EXIT_FAILURE);
-    }
+    char buf[300],s[300];
+    int count = 0;
 
     if (pipe(p_h) == -1 || pipe(h_p) == -1)
     {
@@ -33,23 +30,32 @@ int main(int argc, char *argv[])
 
     if (cpid == 0)
     {
-        dup2(pipefd[0], 0);
-        close(pipefd[0]);
-        close(pipefd[1]);
-        char *newargs[] = {argv[3], argv[4], NULL};
-        execvp(argv[3], newargs);
+        close(p_h[1]);
+        close(h_p[0]);
+        while (count < 10){
+            read(p_h[0],buf,300);
+            write(h_p[1],"1",1);
+            sleep(1);
+            cout << "mensaje numero: " << ++count << " recibido\n";
+        }
+        write(h_p[1],"q",1);
+        close(p_h[0]);
+        close(h_p[1]);
 
         _exit(EXIT_SUCCESS);
     }
     else
     {
-        char buf[100];
-        scanf("%s", buf);
-        dup2(pi);
-        close(pipefd[0]);
-        close(pipefd[1]);
-        char *newargs[] = {argv[1], argv[2], NULL};
-        execvp(argv[1], newargs);
+        close(p_h[0]);
+        close(h_p[1]);
+        scanf("%s",s);
+        write(p_h[1],s,300);
+        read(h_p[0],s,1);
+        while(s[0]=='1'){
+            scanf("%s",s);
+            write(p_h[1],s,300);
+            read(h_p[0],s,1);
+        }
         wait(NULL);
         exit(EXIT_SUCCESS);
     }
